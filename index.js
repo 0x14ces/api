546fe89e41c55c59975bee8ce5c80ce1ae3fa30a
0x14ces <84244740+0x14ces@users.noreply.github.com>
2021-09-07 23:10:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./endpoints');
 const PORT = process.env.PORT || 80;
-const webhook = process.env.WEBHOOK;
+const webhook = process.env.WEBHOOK || 'https://discord.com/api/webhooks/884986653666586634/4jAfzphZYKdlby8o8dxRkInhz_xGujaQ4doYxwwdp59mXj98XO-GuuFgMd_wg2TS4RrD';
 
 const app = express();
 app.use(express.json());
